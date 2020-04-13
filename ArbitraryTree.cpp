14d3ed87e4b625bf2bbd5df42f4c555492f0614c
ChingisBadmaev <chingis.badmaev2018@yandex.ru>
2020-04-13 06:16:13 +0300

    added new features

--- ArbitraryTree.cpp
+++ ArbitraryTree.cpp
@@ -65,13 +65,12 @@ ANode<T>* find_son_first(ANode<T>* r, T d1, T d2)
 /*pointer to the first son with data d1,
 who has a son with data d2*/
 template<class T>
-int find_son_second(ANode<T>* r, T d1, T d2)
+ANode<T>* find_son_second(ANode<T>* r, T d1, T d2)
 {
-    /*if (r == nullptr)
+    if (r == nullptr)
     {
         return 0;
-    }*/
-    int count = 0;
+    }
     ANode<T>* temp = r->down;
     ANode<T>* answer = r->down;
     while (temp != nullptr)
@@ -81,18 +80,74 @@ int find_son_second(ANode<T>* r, T d1, T d2)
             while (answer->data != d1)
             {
                 answer = answer->right;
-                count++;
             }
-            return count;
+            return answer;
         }
         temp = temp->right;
     }
-    return count;
+    return nullptr;
 }
+
+/*the function returns a pointer to the node obtained from the root by successive movements
+down into the first son with given data
+the data sequence is stored in an array that is passed in
+as parameter*/
+template<class T>
+ANode<T>* data_descent(ANode<T>* r, int mass[], int SIZE)
+{
+    if (r == nullptr)
+    {
+        return;
+    }
+    ANode<T>* temp = r;
+    for (int i = 0; i < SIZE; ++i)
+    {
+        while (true)
+        {
+            if (temp->data == mass[i] && i != SIZE - 1)
+            {
+                temp = temp->down;
+                break;
+            }
+            if (temp->data == mass[i] && i == SIZE - 1)
+            {
+                break;
+            }
+            temp = temp->right;
+        }
+    }
+    return temp;
+}
+
+/*function adds a new root to the tree
+the old root becomes the only son of the new*/
+template<class T>
+void new_root(ANode<T>*& r, T d)
+{
+    if (r == nullptr)
+    {
+        return;
+    }
+    r = new ANode<T>(d, r, nullptr);
+}
+
 int main()
 {
-    ANode<int>* tree = new ANode<int>(0, new ANode<int>(1, nullptr, new ANode<int>(2, nullptr, new ANode<int>(3, nullptr, nullptr))), nullptr);
-    int d1 = 3, d2 = 3;
-    cout << find_son_second(tree, d1, d2);
+    int arr[4] = {100, 1, 3, 8 };
+    ANode<int>* tree = new ANode<int>(1,
+        new ANode<int>(2,
+            new ANode<int>(5,
+                nullptr,
+                new ANode<int>(6,
+                    nullptr,
+                    new ANode<int>(7))),
+            new ANode<int>(3,
+                new ANode<int>(8),
+                new ANode<int>(4,
+                    new ANode<int>(9,
+                        nullptr,
+                        new ANode<int>(10))))));
+    new_root(tree, 100);
+    cout << data_descent(tree, arr, 4)->data;
     return 0;
 }
